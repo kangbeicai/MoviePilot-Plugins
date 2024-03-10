7a4276e614568beabef05a4f77a1af39c1651150
kangbeicai <57837294+kangbeicai@users.noreply.github.com>
2024-03-10 16:43:14 +0800

    Update __init__.py
    test

--- plugins/anistrm/__init__.py
+++ plugins/anistrm/__init__.py
@@ -60,9 +60,9 @@ class ANiStrm(_PluginBase):
     # 插件版本
     plugin_version = "2.3"
     # 插件作者
-    plugin_author = "honue"
+    plugin_author = "kangbeicai"
     # 作者主页
-    author_url = "https://github.com/honue"
+    author_url = "https://github.com/kangbeicai"
     # 插件配置项ID前缀
     plugin_config_prefix = "anistrm_"
     # 加载顺序
